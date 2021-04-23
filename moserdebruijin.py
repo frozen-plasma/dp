9add97c6df24833e19fcb181f7a0ade9adbf0816
Wesley <churros8@gmail.com>
2021-04-22 21:09:47 -0500

    python projects so far

---
+++ moserdebruijin.py
@@ -0,0 +1,16 @@
+'''
+Given an integer ‘n’, print the first ‘n’ terms of the Moser-de Bruijn Sequence.
+
+The Moser-de Bruijn sequence is the sequence obtained by adding up the distinct powers of the number 4 (For example 1, 4, 16, 64, etc).
+
+Examples :
+
+Input : 5
+Output : 0 1 4 5 16
+
+Input : 10
+Output : 0 1 4 5 16 17 20 21 64 65
+'''
+
+def mdb(n):
+    
---
+++ newman-conway.py
@@ -0,0 +1,14 @@
+def nc(n):
+    sols = [-1]*(n+1)
+    sols[1] = 1
+    sols[2] = 1
+    for i in range(3, n+1):
+        if sols[i] == -1:
+            sols[i] = sols[sols[i-1]] + sols[i-sols[i-1]]
+
+    print(sols)
+    return sols[-1]
+
+
+print(nc(300))
+
